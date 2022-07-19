b2095984dea2eddc82bc5b38861c0a7fde56fb7b
Vaddi kumar <vaddi.kumar@ibm.com>
2022-07-19 14:20:50 +0530

    Created Xyz and Modified Abc

--- Xyz.java
+++ Xyz.java
@@ -1,13 +1,11 @@
-
-
 public class Abc 
 {
   public static void main(String args[])
  {
     for(int i=1; i<=10; i++)
   {
-   Sysyem.out.println("hello mine");
+   Sysyem.out.println("XYZ");
   }
-   Sysyem.out.println("thanks");
+   Sysyem.out.println("ZXY");
  }
 }
